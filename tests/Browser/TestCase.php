0ebc2823df48e528f30e10c4fbd7c0bcf7968c0f
Josh Hanley <josh@hitsystems.com.au>
2021-01-10 19:22:50 +1100

    Pull in Livewire Dusk Testbench, configure app key, disable unit test suite, and add base browser testcase

---
+++ tests/Browser/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LivewirePackageDemo\Tests\Browser;
+
+use LivewireDuskTestbench\TestCase as LivewireDuskTestbenchTestCase;
+
+class TestCase extends LivewireDuskTestbenchTestCase
+{
+}
