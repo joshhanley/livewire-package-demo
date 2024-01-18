a7982f3f9f72625062030b4405a490e413dddfec
Josh Hanley <josh@hitsystems.com.au>
2024-01-18 20:20:55 +1000

    Remove ray

--- src/LivewirePackageDemoServiceProvider.php
+++ src/LivewirePackageDemoServiceProvider.php
@@ -9,7 +9,6 @@ class LivewirePackageDemoServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        ray('boot');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'lw-pkg-demo');
 
         Livewire::component('counter-component', CounterComponent::class);
